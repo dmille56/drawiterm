ca46f50c66b7c8dbc14a7b1053e95d2c83f8a031
dmille56 <donovanm56@gmail.com>
2026-03-28 17:55:10 -0700

    add myrun command to nix flake for convenience

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  # .
   description = "drawiterm flake";
 
   inputs = {
@@ -13,22 +12,19 @@
       perSystem = { self', pkgs, system, ... }:
       let
         common = import ./nix/common.nix { inherit pkgs; };
+        devcommands = pkgs.writeShellScriptBin "myrun" ''
+          #!${pkgs.bash}/bin/bash
+          python -m drawiterm $*
+        '';
       in 
       {
         devShells.default = pkgs.mkShell {
           packages = common.myBuildPackages ++ common.myDevPackages;
+          buildInputs = [ devcommands ];
 
           shellHook = ''
             # so running the python module runs correctly
             export PYTHONPATH="src"
-
-            # convenience helpers to run the project as a module:
-            # - drawiterm: simple alias
-            # - run-drawiterm: function that forwards arguments
-            alias drawiterm='python -m drawiterm'
-            run-drawiterm() { python -m drawiterm "$@"; }
-            # export function on shells that support it (ignore errors)
-            export -f run-drawiterm 2>/dev/null || true
           '';
         };
         
